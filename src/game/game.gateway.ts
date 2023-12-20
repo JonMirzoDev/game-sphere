bfa6e40fd68937e85b5b43dccded25718dc2c912
JonMirzoDev <islomjon.swe@gmail.com>
2023-12-20 13:55:04 +0500

    game service and gateway created and tictactoe login implemented in service

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,31 +1,83 @@
 import {
   WebSocketGateway,
-  SubscribeMessage,
   WebSocketServer,
+  SubscribeMessage,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  ConnectedSocket,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { GameService } from './game.service'; // Import GameService
 
 @WebSocketGateway()
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
+  constructor(private gameService: GameService) {}
+
   handleConnection(client: Socket, ...args: any[]) {
-    // Handle new connection
+    this.broadcastSessionUpdates(); // Broadcast sessions when a new client connects
   }
 
   handleDisconnect(client: Socket) {
-    // Handle disconnection
+    // Optionally handle disconnection
+  }
+
+  @SubscribeMessage('selectGameType')
+  async handleSelectGameType(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: { gameType: string; playerId: string },
+  ): Promise<void> {
+    try {
+      const session = this.gameService.joinOrCreateSession(
+        payload.gameType,
+        payload.playerId,
+      );
+      client.join(session.id);
+      this.broadcastSessionUpdates(); // Broadcast updated sessions after a new session is joined/created
+    } catch (error) {
+      client.emit('exception', { message: error.message });
+    }
   }
 
   @SubscribeMessage('joinGame')
-  handleJoinGame(client: Socket, payload: any): void {
-    // Logic for joining a game
+  async handleJoinGame(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: { sessionId: string; playerId: string },
+  ): Promise<void> {
+    try {
+      this.gameService.joinGame(payload.sessionId, payload.playerId);
+      client.join(payload.sessionId);
+      this.broadcastSessionUpdates(); // Broadcast updated sessions after a player joins a game
+    } catch (error) {
+      client.emit('exception', { message: error.message });
+    }
   }
 
   @SubscribeMessage('makeMove')
-  handleMakeMove(client: Socket, payload: any): void {
-    // Logic for making a move
+  async handleMakeMove(
+    @ConnectedSocket() client: Socket,
+    @MessageBody()
+    payload: { sessionId: string; playerId: string; position: number },
+  ): Promise<void> {
+    try {
+      const updatedGameState = this.gameService.makeTicTacToeMove(
+        payload.sessionId,
+        payload.playerId,
+        payload.position,
+      );
+      this.server.to(payload.sessionId).emit('gameState', updatedGameState);
+    } catch (error) {
+      client.emit('exception', { message: error.message });
+    }
   }
+
+  // Helper method to broadcast current game sessions to all connected clients
+  private broadcastSessionUpdates() {
+    const availableSessions = this.gameService.getAvailableGameSessions();
+    this.server.emit('availableGames', availableSessions);
+  }
+
+  // Additional methods for other game actions can be added here
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,157 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid'; // For generating unique session IDs
+
+interface GameSession {
+  id: string;
+  players: string[];
+  gameType: string;
+  gameState: any;
+}
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  private gameSessions: Record<string, GameSession> = {};
+
+  constructor() {
+    // Optionally prepopulate some game sessions
+    this.prepopulateSessions();
+  }
+
+  private prepopulateSessions(): void {
+    // Example: Prepopulate 5 Tic-Tac-Toe sessions
+    for (let i = 0; i < 5; i++) {
+      this.createGame(`tic-tac-toe-${i}`, 'tic-tac-toe');
+    }
+    // Add prepopulation for other game types as needed
+  }
+
+  joinOrCreateSession(gameType: string, playerId: string): GameSession {
+    const availableSession = this.findAvailableSession(gameType);
+    if (availableSession) {
+      this.joinGame(availableSession.id, playerId);
+      return availableSession;
+    } else {
+      const newSessionId = this.generateSessionId();
+      return this.createGame(newSessionId, gameType);
+    }
+  }
+
+  private findAvailableSession(gameType: string): GameSession | null {
+    return (
+      Object.values(this.gameSessions).find(
+        (session) =>
+          session.gameType === gameType && session.players.length < 2,
+      ) || null
+    );
+  }
+
+  private createGame(sessionId: string, gameType: string): GameSession {
+    const newGame: GameSession = {
+      id: sessionId,
+      players: [],
+      gameType: gameType,
+      gameState: this.initializeGameState(gameType),
+    };
+    this.gameSessions[sessionId] = newGame;
+    return newGame;
+  }
+
+  joinGame(sessionId: string, playerId: string): GameSession {
+    const game = this.gameSessions[sessionId];
+    if (game && game.players.length < 2) {
+      game.players.push(playerId);
+      return game;
+    }
+    throw new Error('Game not found or already full');
+  }
+
+  private initializeGameState(gameType: string): any {
+    switch (gameType) {
+      case 'tic-tac-toe':
+        return this.initializeTicTacToe();
+      // Add other game types here
+      default:
+        throw new Error('Unknown game type');
+    }
+  }
+
+  private initializeTicTacToe(): any {
+    return {
+      board: Array(9).fill(null), // 3x3 board
+      currentPlayer: 'X', // Player X starts
+      winner: null,
+      draw: false,
+    };
+  }
+
+  makeTicTacToeMove(
+    sessionId: string,
+    playerId: string,
+    position: number,
+  ): any {
+    const game = this.gameSessions[sessionId];
+    if (!game || game.gameType !== 'tic-tac-toe') {
+      throw new Error('Invalid game session');
+    }
+
+    if (
+      game.gameState.currentPlayer !== playerId ||
+      game.gameState.board[position] !== null
+    ) {
+      throw new Error('Invalid move');
+    }
+
+    game.gameState.board[position] = playerId;
+
+    const winner = this.checkTicTacToeWinner(game.gameState.board);
+    if (winner) {
+      game.gameState.winner = winner;
+    } else {
+      if (this.isDraw(game.gameState.board)) {
+        game.gameState.draw = true;
+      } else {
+        game.gameState.currentPlayer =
+          game.gameState.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    }
+
+    return game.gameState;
+  }
+
+  private checkTicTacToeWinner(board: string[]): string | null {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Columns
+      [0, 4, 8],
+      [2, 4, 6], // Diagonals
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+  private isDraw(board: string[]): boolean {
+    return board.every((position) => position !== null);
+  }
+
+  private generateSessionId(): string {
+    return uuidv4(); // Generates a unique session ID
+  }
+
+  // Method to get all available game sessions
+  getAvailableGameSessions(): GameSession[] {
+    return Object.values(this.gameSessions).filter(
+      (session) => session.players.length < 2,
+    );
+  }
+
+  // Additional methods for other game types can be added here
+}
